31e4e37e262d62d610eba882d6c69c800a462f26
Katie Maxwell <katie@codeinstitute.net>
2017-11-15 12:53:25 +0000

    login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import logout
+from .views import logout, login
 
 urlpatterns = [
-    url(r'^logout', logout)
+    url(r'^logout', logout),
+    url(r'^login', login),
     ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages, auth
+from .forms import UserLoginForm
 
 # Create your views here.
 def get_index(request):
@@ -10,3 +11,22 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You have successfully logged out')
     return redirect(get_index)
+    
+def login(request):
+    if request.method=="POST":
+        form=UserLoginForm(request.POST)
+        
+        if form.is_valid():
+            user = auth.authenticate(username=form.cleaned_data['username'],
+                                     password=form.cleaned_data['password'])
+
+            if user is not None:
+                auth.login(request, user)
+            else:
+                form.add_error(None, "Your username or password was not recognised")
+                
+            return redirect(get_index)
+    else:
+        form = UserLoginForm()
+    
+    return render(request, "login.html", {'form': form})
