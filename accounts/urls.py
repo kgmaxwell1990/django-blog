7eb7dd62d166d7f09e262b8efb8b003acc047647
Katie Maxwell <katie@codeinstitute.net>
2017-11-15 14:11:10 +0000

    register

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import logout, login
+from .views import logout, login, register
 
 urlpatterns = [
     url(r'^logout', logout),
     url(r'^login', login),
+    url(r'^register', register),
     ]
